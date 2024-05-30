012e209e7f8da0f7ddb748fd315e569db4df4306
Ho <dt.gael.ho@gmail.com>
2024-05-30 11:48:56 +0200

    exo2, if et for

---
+++ AlgoPHP/exo2.php
@@ -0,0 +1,25 @@
+<h1>Exercice </h1>
+
+<p>A partir de la phrase de l’exercice 1, écrire l’instruction permettant de compter le nombre de mots
+contenus dans celle-ci.</p>
+
+<h2>Résultat</h2>
+
+<?php
+
+$phrase = "Notre formation DL commence aujourd'hui";
+$nbCaracteres = strlen($phrase);
+$mots = 0;
+
+if ($nbCaracteres != 0){
+    $mots = 1;
+    for ($i = 0; $i <= $nbCaracteres; $i++) {
+        if ($phrase[$i] == " "){
+            $mots++;
+        }
+        
+    }
+}
+
+
+echo "La phrase $phrase contient $mots mots";
