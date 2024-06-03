89330622dad76eb41a69fcec4a62d4ed9194229d
Ho <dt.gael.ho@gmail.com>
2024-06-03 16:25:46 +0200

    Exo 3, lien hypertexte dans le tableau

---
+++ AlgoPHP2/exo4.php
@@ -0,0 +1,44 @@
+<h1>Exercice 4</h1>
+
+<p>A partir de l’exercice 2, ajouter une colonne supplémentaire dans le tableau HTML qui contiendra
+le lien hypertexte de la page Wikipédia de la capitale (le lien hypertexte devra s’ouvrir dans un
+nouvel onglet et le tableau sera trié par ordre alphabétique de la capitale).</p>
+
+<h2>Résultat</h2>
+
+<?php
+
+$capitales = array (
+    "France"=>"Paris",
+    "Allemagne"=>"Berlin",
+    "USA"=>"Washington",
+    "Italie"=>"Rome",
+    "Espagne"=>"Madrid");
+
+//afficher le résultat de la fonction cafficherTablesHTML
+echo afficherTableHTML($capitales);
+
+
+function afficherTableHTML($capitales) {
+    asort($capitales);
+    $result = "<table border=1>
+                <thead>
+                    <tr>
+                        <th>Pays</th>
+                        <th>Capitales</th>
+                        <th>Lien wiki</th>
+                    </tr>
+                </thead>
+            <tbody>";
+
+    foreach ($capitales as $pays => $capitale){
+        $result = "$result"."<tr>
+                        <td>".mb_strtoupper($pays)."</td>
+                        <td>".ucfirst($capitale)."</td>
+                        <td>"."<a href=https://fr.wikipedia.org/wiki/".$capitale." target='_blank'>Lien</a>"."</td>
+                    </tr>";
+    }
+
+    $result = "$result"."</tbody></table>";
+    return $result;
+}
