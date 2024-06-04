fa82f42457a62eb7bd784afb453a8b4484524298
Ho <dt.gael.ho@gmail.com>
2024-06-04 09:47:50 +0200

    exo9, radiobutton

---
+++ AlgoPHP2/exo9.php
@@ -0,0 +1,22 @@
+<h1>Exercice 6</h1>
+
+<p>Créer une fonction personnalisée permettant d’afficher des boutons radio avec un tableau de
+valeurs en paramètre ("Monsieur","Madame","Mademoiselle").</p>
+
+<h2>Résultat</h2>
+
+<?php
+
+
+$nomsRadio = array("Monsieur","Madame","Mademoiselle");
+//afficher le résultat de la fonction alimenterListeDeroulante
+echo AfficherRadio($nomsRadio);
+
+
+function AfficherRadio($nomsRadio) {
+    foreach ($nomsRadio as $nomsRadio){//le "name" est là pour liée les radiobutton entre eux
+        $result .= "<input type='radio' id='$nomsRadio' name='genre'/>
+        <label for='$nomsRadio'>$nomsRadio</label></br>";
+    }
+    return $result;
+}
